eb906df1d0c5ce878bb1f8e411e0f95df655a8e9
Mario Palacios <mepvdevelopments@gmail.com>
2021-01-31 15:31:13 -0300

    adding jsonplaceholderAPI and refactoring

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './App.css'
+import './containers/App.css'
 import reportWebVitals from './reportWebVitals';
-import App from "./App";
+import App from "./containers/App";
 import 'tachyons';
 
 ReactDOM.render(
