c77a6c1ef5a90090c1d201be3a1153f6f0aa5b53
Mario Palacios <mepvdevelopments@gmail.com>
2021-01-31 21:17:41 -0300

    new faces

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ id, name, email }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robot card' src={`https://robohash.org/${id}?200x200`} />
+            <img alt='robot card' src={`https://robohash.org/${id}?set=set3`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
